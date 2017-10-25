762be31c3ccb80d97d6a71d041c743edca32d020
Alfred Datui <alfred@autoarmour.co>
2017-10-25 19:01:34 +0800

    First Commit

---
+++ php/login-requester.php
@@ -0,0 +1,63 @@
+<?php
+  //Start the Session
+  session_start();
+  require('connect.php');
+
+  $usernameErr = "";
+
+  //3. If the form is submitted or not.
+  //3.1 If the form is submitted
+  if (isset($_POST['login'])) {
+    //update action
+    if (isset($_POST['username']) and isset($_POST['password'])) {
+      //3.1.1 Assigning posted values to variables.
+      $username = $_POST['username'];
+      $password = $_POST['password'];
+  
+      //3.1.2 Checking the values are existing in the database or not
+      $query = "SELECT * FROM `login` WHERE username='$username' and password='$password'";
+      $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+      $count = mysqli_num_rows($result);
+  
+      //3.1.2 If the posted values are equal to the database values, then session will be created for the user.
+      if ($count == 1) {
+        $_SESSION['username'] = $username;
+      } else {
+      //3.1.3 If the login credentials doesn't match, he will be shown with an error message.
+        $fmsg = "Invalid Login Credentials.";
+        $usernameErr = "username or passowrd is either incorrect or have not been registered yet";
+        $_SESSION['username'] = NULL;
+        $_SESSION['message'] = $usernameErr;
+      }
+    } else {
+      $usernameErr = "username or passowrd is required";
+      $_SESSION['message'] = $usernameErr;
+    }
+  
+    //3.1.4 if the user is logged in Greets the user with message
+    if (isset($_SESSION['username'])) {
+      header("Location: querytask.php");
+    } else {
+      header("Location: ../html/homepage.html");
+    }
+    //3.2 When the user visits the page first time, simple login form will be displayed.
+  } else if (isset($_POST['register'])) {
+      //delete action
+      if (isset($_POST['username']) && isset($_POST['password'])){
+        $username = $_POST['username'];
+        $email = "Alfred1datui@gmail.com";
+        $password = $_POST['password'];
+          
+        echo $query = "INSERT INTO `login` (username, password, email) VALUES ('$username', '$password', '$email')";
+        $result = mysqli_query($connection, $query);
+          
+        if($result){
+            $msg = "User Created Successfully.";
+        }else{
+            $msg ="User Registration Failed";
+        }
+        $_SESSION['message'] = $msg;
+        header("Location: ../html/homepage.html");
+      }
+  }
+?>
---
+++ php/querytask.php
@@ -0,0 +1,34 @@
+<?php
+  session_start();
+
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "profile";
+  // Create connection
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+  // Check connection
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  $userName = $_SESSION['username'];
+
+  $sql = "SELECT * FROM request WHERE USER='" . $userName . "'";
+  
+  if (mysqli_query($conn, $sql)) {
+    $request = mysqli_query($conn, $sql);
+    $all_query = "";
+    while($row = $request->fetch_assoc()) {
+      $all_query = $all_query . "<tr>";
+      $all_query = $all_query . "<td><center><font color='green'>" . $row['TASKTITLE'] . "</font></center></td>";
+      $all_query = $all_query . "<td><center>" . $row['COMPLETION_OF_TASK'] . "</center></td>";
+      $all_query = $all_query . "<td><center><font color='red'>" . $row['PAYMENT_PROCESS'] . "</font></center></td>";
+      $all_query = $all_query . "<td><center><button>Download</button></center></td>";
+      $all_query = $all_query . "<tr>";
+    }
+    $_SESSION['querytask'] = $all_query;
+    header("Location: ../html/requester-home.html");
+  } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }  
+?>
